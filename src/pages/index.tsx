f34650be6d22c048ec352d38158a1eb056c996e6
Ayrton Souza <ayrtonsouza@icloud.com>
2021-07-22 17:16:22 -0300

    [:sparkles:] using ssg to fetch product price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe'
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
 }
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JG7xkLAgEDrMmHvqVF06LHO')
   const product = {
     priceId: price.id,
@@ -51,6 +51,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24,
   }
 }
