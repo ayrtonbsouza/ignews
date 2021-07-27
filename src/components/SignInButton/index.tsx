388546297f414ba62596bd8ce414dafb68146534
Ayrton Souza <ayrtonsouza@icloud.com>
2021-07-27 15:49:43 -0300

    [:sparkles:] authentication with github development

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,21 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession()
+  console.log(session)
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton} type="button">
+  return session ? (
+    <button className={styles.signInButton} onClick={() => signOut()} type="button">
       <FaGithub color="#04d361" />
-      Ayrton Souza
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button className={styles.signInButton} type="button">
+    <button className={styles.signInButton} onClick={() => signIn('github')} type="button">
       <FaGithub color="#eba417" />
       Sign In with Github
     </button>
