e3ed07a9177f05e4197136ba415affd58e13ba4f
Ayrton Souza <ayrtonsouza@icloud.com>
2021-07-29 15:03:25 -0300

    [:sparkles:] setting routes on header buttons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import { SignInButton } from '../SignInButton';
 
 import styles from './styles.module.scss';
@@ -6,10 +8,18 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <Link href="/">
+          <a>
+            <Image src="/images/logo.svg" alt="ig.news" width={110} height={31} />
+          </a>
+        </Link>
         <nav>
-          <a className={styles.active} href="">Home</a>
-          <a href="">Posts</a>
+          <Link href="/">
+            <a className={styles.active} >Home</a>
+          </Link>
+          <Link href="/posts">
+            <a>Posts</a>
+          </Link>
         </nav>
         <SignInButton />
       </div>
